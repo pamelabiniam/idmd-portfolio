a7fa89773d71db30f58e7a0f478f5ab4cdcfaafe
pamelabiniam <60490172+pamelabiniam@users.noreply.github.com>
2020-03-12 14:08:38 -0700

    Add files via upload

---
+++ Image Example/sketch.js
@@ -0,0 +1,14 @@
+var pup;
+
+function preload() {
+  pup = loadImage("puppy.jpg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+var a = 789/526;
+function draw() {
+  background(220);
+  image(pup, 0, 0, 400, 400/a)
+}
