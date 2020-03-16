e4a2fe8ac37c04ade3ef944ceb2e34e39860f7de
pamelabiniam <60490172+pamelabiniam@users.noreply.github.com>
2020-03-16 14:42:06 -0700

    Add files via upload

---
+++ Crosshatch/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225);
+  for (i = 0; i < mouseX; i += 5) {
+    for (j = 0; j < mouseY; j += 5) {
+      line(0, i, mouseX, i);
+      line(j,0, j, mouseY)
+    }
+  }
+
+}
