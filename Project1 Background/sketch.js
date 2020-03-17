8ced87d7c6e695b310af86b6e2c5abd970e78228
pamelabiniam <60490172+pamelabiniam@users.noreply.github.com>
2020-03-16 18:27:25 -0700

    Add files via upload

--- Project1 Background/sketch.js
+++ Project1 Background/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
   createCanvas(300, 300);
 }
-
+//paramaters for canvas (width, height)
 function draw() {
   background(214,159,159);
 }
+//Paramaters for background (Red, Green, Blue)
+//My First Exercise, learning RGB and how to change the background!!
